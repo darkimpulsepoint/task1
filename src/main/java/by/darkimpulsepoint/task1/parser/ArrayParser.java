a28e32fda2e26c2aac9338192387504b33c531b7
Alex <darkimpulsepoint@gmail.com>
2026-05-11 18:58:27 +0300

    refactoring

---
+++ src/main/java/by/darkimpulsepoint/task1/parser/ArrayParser.java
@@ -0,0 +1,7 @@
+package by.darkimpulsepoint.task1.parser;
+
+import by.darkimpulsepoint.task1.entity.IntegerArray;
+
+public interface ArrayParser {
+    IntegerArray parseLine(String line);
+}
--- src/main/java/by/darkimpulsepoint/task1/parser/impl/IntegerArrayParser.java
+++ src/main/java/by/darkimpulsepoint/task1/parser/impl/IntegerArrayParser.java
@@ -1,11 +1,13 @@
 package by.darkimpulsepoint.task1.parser.impl;
 
 import by.darkimpulsepoint.task1.entity.IntegerArray;
+import by.darkimpulsepoint.task1.parser.ArrayParser;
 import java.util.Arrays;
 
-public class IntegerArrayParser {
+public class IntegerArrayParser implements ArrayParser {
     private static final String WHITESPACE_REGEX = "\\s+";
 
+    @Override
     public IntegerArray parseLine(String line) {
         int[] values = Arrays.stream(line.strip().split(WHITESPACE_REGEX))
                 .mapToInt(Integer::parseInt)
